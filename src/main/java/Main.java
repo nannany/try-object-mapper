adce99814c849dcb2efd67772c753fe7b427a215
nannany <ymym1990ymym@gmail.com>
2022-02-23 18:31:28 +0900

    disablesするコード追加

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import enums.Sex;
@@ -13,14 +16,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         ObjectMapper normalObjectMapper = new ObjectMapper();
+        ObjectMapper disableFailOnUnknownPropertiesObjectMapper = new ObjectMapper()
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
         var expected = new Human("翔平", "大谷", Sex.MALE);
         assertThat(normalObjectMapper.readValue(new File("src/main/resources/man.json"), Human.class))
                 .isEqualTo(expected);
 
-        // 余計なkey-valueが入っている場合
+        // 余計なkey-valueが入っている
+        // 特別な設定をしていないObjectMapper
         assertThatThrownBy(() -> normalObjectMapper.readValue(new File("src/main/resources/has-unknown.json"), Human.class))
                 .isExactlyInstanceOf(UnrecognizedPropertyException.class);
+
+        // 余計なkey-valueが入っている
+        assertThat(disableFailOnUnknownPropertiesObjectMapper.readValue(new File("src/main/resources/has-unknown.json"), Human.class))
+                .isEqualTo(expected);
+
 
 
     }
