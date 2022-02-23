5bd6b5e7d2e723da76f8f31e78a09cd327e98714
nannany <ymym1990ymym@gmail.com>
2022-02-23 18:04:26 +0900

    assertしてやる

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,22 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
+import enums.Sex;
 import record.Human;
 
 import java.io.File;
 import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         ObjectMapper normalObjectMapper = new ObjectMapper();
 
-        var otani = normalObjectMapper.readValue(new File("src/main/resources/man.json"), Human.class);
-        System.out.println(otani);
+        var expected = new Human("翔平", "大谷", Sex.MALE);
+        assertThat(normalObjectMapper.readValue(new File("src/main/resources/man.json"), Human.class))
+                .isEqualTo(expected);
+        System.out.println(expected);
+
 
 
     }
